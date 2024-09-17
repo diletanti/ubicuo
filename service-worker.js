ae7c4ce78edc8b017aadc3c7e39b6a4bbb85f7a3
diletanti <jgarsan455@g.educaand.es>
2024-09-17 10:54:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/',
           'https://script.google.com/a/macros/g.educaand.es/s/AKfycbzATIVH2Enw4z7UCDupTYRQ6kdcatrf3sbDXVdGq4MhhyIGH6aIyJg-R9I_pbaTDAuf/exec',
-          'https://blogsaverroes.juntadeandalucia.es/iesantonioalvarezlopez/files/2024/09/ubi-192.png',
-          'https://blogsaverroes.juntadeandalucia.es/iesantonioalvarezlopez/files/2024/09/ubi-512.png',
+          'https://github.com/diletanti/ubicuo/blob/main/ubi-192.png',
+          'https://github.com/diletanti/ubicuo/blob/main/ubi-512.png',
           // Añade otros archivos que quieras almacenar en caché
         ]);
       })
